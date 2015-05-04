f5c6c15954ab3a303a2a6caf5a745d3a16cef567
Ryan Paglinawan <rymotion1@me.com>
2015-05-04 11:11:06 -0700

    I kind of got it
    I just need the code to see the function

--- Lab2_int.cpp
+++ Lab2_int.cpp
@@ -34,12 +34,27 @@ Search for "abc" in range 1 to 4.  Return index if found or return  -1 if not fo
 #include <iostream>
 #include <vector>
 #include <string>
-#include "search.h"
 
-template <typename T> 
+using namespace std;
+
+int main(){
+	int intarr[9] = {1, 2, 6, 7, 10, 22, 19, 33, 35};
+	vector <int> intcck(intarr, intarr + 9);//	This will create the vector to check the int
+
+	string strarr[6] = {"abs", "awp", "bhg", "sghsh", "het", "hetet"};
+	vector <string> strcck(strarr, strarr + 6);//	This will create the vecctor to check the string
+
+	seqSearch(intcck, intcck.begin(), intcck.end(), 10);
+
+	intcck.seqSearch(intcck, intcck.begin(), intcck.end(), 10, 6);
+	strcck.seqSearch(strcck, strcck.begin(), strcck.end(), "het", strcck.size());
+	strcck.seqSearch(strcck, strcck.begin(), strcck.end(), "abc", 4);
+}
+
+template <typename T>
 int seqSearch(const vector<T>& v, T first, T last, const T& target, const int range){
 	//int range in this respect is going to be the range that the system will search for the value
-	vector <T> :: const_iteration iter;
+	//vector <T> :: const_iteration iter;
 	for (int i = 0; i < v.size(); i++) {
 		if (i == range) {
 			if (target == v[i]) {
@@ -51,20 +66,4 @@ int seqSearch(const vector<T>& v, T first, T last, const T& target, const int ra
 			}
 		}
 	}
-}
-
-using namespace std;
-
-int main(){
-	vector <int> intcck(intarr);//	This will create the vector to check the int
-	int intarr[] = {1, 2, 6, 7, 10, 22, 19, 33, 35};
-
-	vector <string> strcck(strarr);//	This will create the vecctor to check the string
-	string strarr[] = {"abs", "awp", "bhg", "sghsh", "het", "hetet"};
-
-	seqSearch(intcck, intcck.begin(), intcck.end(), 10);
-
-	intcck.seqSearch(intcck, intcck.begin(), intcck.end(), 10, 6);
-	strcck.seqSearch(strcck, strcck.begin(), strcck.end(), "het", strcck.size());
-	strcck.seqSearch(strcck, strcck.begin(), strcck.end(), "abc", 4);
 }
