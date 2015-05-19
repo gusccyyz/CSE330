258ce45d20688789b7c0a4ade270f88362a7fd8f
Ryan Paglinawan <rymotion1@me.com>
2015-05-19 16:12:52 -0700

    update
    update

--- Lab4_Paglinawan_Hlatshwayo/lab4.cpp
+++ Lab4_Paglinawan_Hlatshwayo/lab4.cpp
@@ -18,14 +18,23 @@ int bubble(vector<int> x);
 int insertion(vector<int> x);
 
 int main(){
-	vector<int> ();
+	int n;
 
 	cout << "Something for the user: \n";
+	cin >> n;
+
+	vector<int> x(n);
+
+	selection(x);
+	bubble(x);
+	insertion(x);
 	
 	return 0;
 }
 
 int selection(vector<int> x){//selection sort
+	int i;
+	int j;
 	for (i = 0; i < n-1; i++){
 	    for (j = i+1; j < n; j++){
 	        if (v[i] > v[j]){
@@ -33,9 +42,11 @@ int selection(vector<int> x){//selection sort
 	        }
 	    }
 	}
-	return
+	//This should spit out the time that it takes to finish this operation
 }
 int bubble(vector<int> x){//bubble sort
+	int i;
+	int j;
 	for (i = n-1; i > 0; i--){
 	    for (j = 0; j < i; j++){
 	        if (v[j] > v[j+1]){
@@ -43,14 +54,16 @@ int bubble(vector<int> x){//bubble sort
 	        }
 	    }
 	}
-	return
+	//This should spit out the time that it takes to finish this operation
 }
 int insertion(vector<int> x){//insertion sort
+	int i;
+	int j;
 	for (i = 1; i < n; i++){
 	    for (elem = v[i], j = i-1; j >=0 and elem < v[j]; j--){
 	        v[j+1] = v[j];
 	    }
 	    v[j+1] = elem;
 	}
-	return
+	//This should spit out the time that it takes to finish this operation
 }
